8af655a13e0e75c1b33c2b3c8ac608389cbb132d
betomcp <beto.porto2000@gmail.com>
2023-09-29 14:22:29 -0300

    controller de volta

--- src/spe/auth/auth.module.ts
+++ src/spe/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { RsUserModule } from "src/resource/rs-user/rs-user.module";
+import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
-  controllers: [],
+  controllers: [AuthController],
   providers: [AuthService],
   imports: [RsUserModule],
 })
