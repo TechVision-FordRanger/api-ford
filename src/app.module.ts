e91b6e3ede237a24f03e4579d0d12a0532981f23
betomcp <beto.porto2000@gmail.com>
2023-09-29 14:16:27 -0300

    ajuste ep

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppService } from './app.service';
-import { RsPanelSymbolsModule } from './resource/rs-panel-symbols/rs-panel-symbols.module';
-import { RsRangerModelModule } from './resource/rs-ranger-model/rs-ranger-model.module';
-import { RsAddressModule } from './resource/rs-address/rs-address.module';
-import { RsUserModule } from './resource/rs-user/rs-user.module';
-import { User } from './resource/rs-user/entities/rs-user.entity';
-import { OnboardingModule } from './spe/onboarding/onboarding.module';
-import { AuthModule } from './spe/auth/auth.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppService } from "./app.service";
+import { RsPanelSymbolsModule } from "./resource/rs-panel-symbols/rs-panel-symbols.module";
+import { RsRangerModelModule } from "./resource/rs-ranger-model/rs-ranger-model.module";
+import { RsAddressModule } from "./resource/rs-address/rs-address.module";
+import { RsUserModule } from "./resource/rs-user/rs-user.module";
+import { User } from "./resource/rs-user/entities/rs-user.entity";
+import { OnboardingModule } from "./spe/onboarding/onboarding.module";
+import { AuthModule } from "./spe/auth/auth.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: 'rm86750-fiap.database.windows.net',
-      database: 'db-ford',
-      username: 'techvision',
-      password: 'ford@2023',
+      type: "mssql",
+      host: "rm86750-fiap.database.windows.net",
+      database: "db-ford",
+      username: "techvision",
+      password: "ford@2023",
       extra: {
         trustServerCertificate: true,
         encrypt: true,
