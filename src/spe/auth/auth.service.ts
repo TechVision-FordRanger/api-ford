bbb3188850fc335710fb45ea2a4f67c0f382ee1d
betomcp <beto.porto2000@gmail.com>
2023-10-09 03:45:28 -0300

    renavam

--- src/spe/auth/auth.service.ts
+++ src/spe/auth/auth.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
-import { RsUserService } from "src/resource/rs-user/rs-user.service";
-import { DefaultResponseDto } from "src/shared/dto/default-response.dto";
-import { AuthDto } from "./dto/auth.dto";
+import { Injectable } from '@nestjs/common';
+import { RsUserService } from 'src/resource/rs-user/rs-user.service';
+import { DefaultResponseDto } from 'src/shared/dto/default-response.dto';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
@@ -11,16 +11,16 @@ export class AuthService {
     try {
       const user = await this.rsUserService.findOneByEmail(authObj.email);
       if (!user) {
-        return new DefaultResponseDto(false, "Usuário não encontrado", null);
+        return new DefaultResponseDto(false, 'Usuário não encontrado', null);
       }
 
       if (user.password != authObj.password) {
-        return new DefaultResponseDto(false, "Senha incorreta", null);
+        return new DefaultResponseDto(false, 'Senha incorreta', null);
       }
 
-      return new DefaultResponseDto(true, "sucesso!", user);
+      return new DefaultResponseDto(true, 'sucesso!', user);
     } catch (error) {
-      return new DefaultResponseDto(false, "erro ao fazer log in", error);
+      return new DefaultResponseDto(false, 'erro ao fazer log in', error);
     }
   }
 }
--- src/spe/onboarding/onboarding.service.ts
+++ src/spe/onboarding/onboarding.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from "@nestjs/common";
-import { CreateRsUserDto } from "src/resource/rs-user/dto/create-rs-user.dto";
-import { UpdateRsUserDto } from "src/resource/rs-user/dto/update-rs-user.dto";
-import { RsUserService } from "src/resource/rs-user/rs-user.service";
-import { DefaultResponseDto } from "src/shared/dto/default-response.dto";
-import { v4 as uuid } from "uuid";
+import { Injectable } from '@nestjs/common';
+import { CreateRsUserDto } from 'src/resource/rs-user/dto/create-rs-user.dto';
+import { UpdateRsUserDto } from 'src/resource/rs-user/dto/update-rs-user.dto';
+import { RsUserService } from 'src/resource/rs-user/rs-user.service';
+import { DefaultResponseDto } from 'src/shared/dto/default-response.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class OnboardingService {
@@ -12,32 +12,55 @@ export class OnboardingService {
   async createUser(createRsUserDto: CreateRsUserDto) {
     try {
       createRsUserDto.uuid = uuid();
-      createRsUserDto.password = "onboarding";
+      createRsUserDto.password = 'onboarding';
 
       const newUser = await this.rsUserService.create(createRsUserDto);
 
       return new DefaultResponseDto(
         true,
-        "Usuário cadastrado com sucesso",
+        'Usuário cadastrado com sucesso',
         newUser,
       );
     } catch (error) {
-      return new DefaultResponseDto(false, "Erro ao criar usuário", error);
+      return new DefaultResponseDto(false, 'Erro ao criar usuário', error);
     }
   }
 
   async updateUser(id: number, updateRsUserDto: UpdateRsUserDto) {
     try {
+      if (updateRsUserDto.renavam) {
+        updateRsUserDto = this.checkRenavam(updateRsUserDto);
+      }
+
       await this.rsUserService.update(id, updateRsUserDto);
       const updatedUser = await this.rsUserService.findOne(id);
 
       return new DefaultResponseDto(
         true,
-        "Usuário atualizado com sucesso",
+        'Usuário atualizado com sucesso',
         updatedUser,
       );
     } catch (error) {
-      return new DefaultResponseDto(false, "Erro ao atualizar usuário", null);
+      return new DefaultResponseDto(false, 'Erro ao atualizar usuário', null);
+    }
+  }
+
+  private checkRenavam(updateRsUserDto: UpdateRsUserDto): UpdateRsUserDto {
+    if (
+      parseInt(
+        updateRsUserDto.renavam.substring(
+          updateRsUserDto.renavam.length - 1,
+          updateRsUserDto.renavam.length,
+        ),
+      ) %
+        2 ==
+      0
+    ) {
+      updateRsUserDto.rangerColor = 'blue';
+      return updateRsUserDto;
+    } else {
+      updateRsUserDto.rangerColor = 'red';
+      return updateRsUserDto;
     }
   }
 }
